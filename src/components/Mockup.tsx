01cd60e9459d4a7b0c30bdc4dc798cdd2e9923d6
DamianLtz <damianlaterza@gmail.com>
2022-04-30 15:38:26 -0300

    v0.4

--- src/components/Mockup.tsx
+++ src/components/Mockup.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import MockupImg from "../img/mockups/mockup-home.svg";
 
 const Mockup = () => {
-  return <img src={MockupImg} alt="" className="img-fluid mockup" />;
+  return <img src={MockupImg} alt="" className="img-fluid mockup w-100" />;
 };
 
 export default Mockup;
