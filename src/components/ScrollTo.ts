234a18988730947020db5e277ce6eccb98fb661f
DamianLtz <damianlaterza@gmail.com>
2022-05-02 16:40:01 -0300

    v0.7

--- src/components/ScrollTo.ts
+++ src/components/ScrollTo.ts
@@ -1,8 +1,12 @@
 const scrollTo = () => {
   if (window.screen.width <= 991) {
     window.scrollTo(0, 550);
+  } else if (window.screen.width <= 1272) {
+    window.scrollTo(0, 500);
+  } else if (window.screen.width <= 1366) {
+    window.scrollTo(0, 525);
   } else {
-    window.scrollTo(0, 450);
+    window.scrollTo(0, 575);
   }
 };
 
