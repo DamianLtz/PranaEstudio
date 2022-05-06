18225a2cdf8a43729e4fb5a2a792d4b87c7012b0
DamianLtz <damianlaterza@gmail.com>
2022-05-05 23:15:25 -0300

    v0.8

--- src/components/Mockup.tsx
+++ src/components/Mockup.tsx
@@ -1,8 +1,23 @@
 import React from "react";
-import MockupImg from "../img/mockups/mockup-home.svg";
+import mockup1 from "../img/mockups/prana-work-1.png";
+import mockup2 from "../img/mockups/prana-work-2.png";
+import mockup3 from "../img/mockups/prana-work-3.png";
 
 const Mockup = () => {
-  return <img src={MockupImg} alt="" className="img-fluid mockup w-100" />;
+  return (
+    // <div className="d-flex justify-content-center">
+    //   <img src={mockup1} alt="" className="img-fluid w-100 mockup"/>
+    //   <img src={mockup2} alt="" className="img-fluid w-100" />
+    //   <img src={mockup3} alt="" className="img-fluid w-100"/>
+    // </div>
+    <div className="row">
+      <div className="col-lg-12">
+        <img src={mockup1} alt="" className="img-fluid w-33 mockup" />
+        <img src={mockup2} alt="" className="img-fluid w-33" />
+        <img src={mockup3} alt="" className="img-fluid w-33" />
+      </div>
+    </div>
+  );
 };
 
 export default Mockup;
